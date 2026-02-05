bf88ceecc3bb8c2326b7706d028b90c1316e815f
MaksStud <173271073+StuditskyiMaksymITH@users.noreply.github.com>
2026-02-05 14:57:03 +0200

    UPDATED: enhanced voice ID retrieval in get_voice.py by adding logging for better traceability and improved documentation with a docstring.

--- handlers/get_voice.py
+++ handlers/get_voice.py
@@ -1,11 +1,18 @@
+import logging
 from aiogram import F, types
 from aiogram import Router
 
+logger = logging.getLogger(__name__)
 router = Router()
 
 
 @router.message(F.voice)
 async def get_voice_id(message: types.Message):
+    """
+    Extract and return the voice file_id.
+
+    :param message: Telegram message with voice.
+    """
     file_id = message.voice.file_id
-    await message.answer(f"Твій ID для settings.py:\n\n<code>{file_id}</code>", parse_mode="HTML")
-    print(f"Отримано ID: {file_id}")
+    await message.answer(f"ID for voice message:\n\n<code>{file_id}</code>", parse_mode="HTML")
+    logger.info(f"ID for voice message: {file_id}")
